76b2737dd31503a758a694a49b8d0fe9eb41a115
xdlegendyt4k <105623973+xdlegendyt4k@users.noreply.github.com>
2022-05-16 12:38:22 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
- ffmpeg -stream_loop -1 -re -thread_queue_size 5512 -an -i "video.mp4" -stream_loop -1 -re -i "http://stream.zeno.fm/v4wf5ezpxrhvv" -stream_loop -1 -re -i -c:v copy -c:a copy -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+ ffmpeg -stream_loop -1 -re -thread_queue_size 5512 -an -i "ok.mp4" -stream_loop -1 -re -i "http://stream.zeno.fm/v4wf5ezpxrhvv" -stream_loop -1 -re -i -c:v copy -c:a copy -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
 done
