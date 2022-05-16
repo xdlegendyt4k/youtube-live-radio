e29e73a2add798f06b267c126cd6ecd3c488d5d1
xdlegendyt4k <105623973+xdlegendyt4k@users.noreply.github.com>
2022-05-16 12:16:43 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
-ffmpeg -stream_loop -1 -re -i video.mp4 -stream_loop -1 -re -i http://stream.zeno.fm/v4wf5ezpxrhvv -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 500k -f flv  rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+ffmpeg -stream_loop -1 -re -i ok.mp4 -stream_loop -1 -re -i http://stream.zeno.fm/v4wf5ezpxrhvv -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 500k -f flv  rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
 done
